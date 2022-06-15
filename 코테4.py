df39918d6842b627571121dedd85314fcb629da4
Yoon8937 <cyg8937@gmail.com>
2022-06-15 11:06:40 +0900

    2022년 06월 15일

---
+++ 코테4.py
@@ -0,0 +1,28 @@
+# while문으로 별찍기 2개 하기...
+
+
+# #중복 된 수 제거
+# arr = [39, 40, 41, 0, 1, 2, 40, 41, 0, 1]
+# ans = []
+# for i in range(len(arr)):
+#     if not arr[i] in ans:
+#         ans.append(arr[i])
+# print(ans)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
